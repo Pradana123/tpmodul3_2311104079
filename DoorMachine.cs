0fc95a03427d3d109ee9911ca46751f8a588e5ab
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-03-04 11:06:05 +0700

    Create DoorMachine.cs

---
+++ DoorMachine.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_2311104071
+{
+    class DoorMachine
+    {
+        private enum State { Terkunci, Terbuka }
+
+        private State currentState;
+
+
+
+        public DoorMachine()
+
+        {
+
+            currentState = State.Terkunci;
+
+            Console.WriteLine("Pintu terkunci");
+
+        }
+
+
+
+        public void BukaPintu()
+
+        {
+
+            if (currentState == State.Terkunci)
+
+            {
+
+                currentState = State.Terbuka;
+
+                Console.WriteLine("Pintu tidak terkunci");
+
+            }
+
+        }
+
+
+
+        public void KunciPintu()
+
+        {
+
+            if (currentState == State.Terbuka)
+
+            {
+
+                currentState = State.Terkunci;
+
+                Console.WriteLine("Pintu terkunci");
+
+            }
+
+        }
+
+    }
+}
