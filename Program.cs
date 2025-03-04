cfa6ef7571e00275be0757e8a0e5a1c9e65a06f2
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-03-04 11:01:41 +0700

    2 Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul3_2311104071;
+
+class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        KodePos kodePos = new KodePos();
+
+        Console.WriteLine(kodePos.getKodePos("Margasari")); // Output: 40266
+
+
+
+    }
+
+}
