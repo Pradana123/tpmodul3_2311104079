9edb59ca94dcf370d0d082e69b375abaff2b3e12
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-03-04 11:02:36 +0700

    Create KodePos.cs

---
+++ KodePos.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_2311104071
+{
+    class KodePos
+    {
+        public string getKodePos(string kelurahan)
+
+        {
+
+            switch (kelurahan)
+
+            {
+
+                case "Batumunggal": return "40266";
+
+                case "Kujangsari": return "40287";
+
+                case "Mengger": return "40267";
+
+                case "Wates": return "40256";
+
+                case "Cijaura": return "40287";
+
+                case "Jatisari": return "40286";
+
+                case "Margasari": return "40286";
+
+                case "Sekejati": return "40286";
+
+                case "Kebonwaru": return "40272";
+
+                case "Maleer": return "40274";
+
+                case "Samoja": return "40273";
+
+                default: return "Kode pos tidak ditemukan";
+
+            }
+
+        }
+
+    }
+}
